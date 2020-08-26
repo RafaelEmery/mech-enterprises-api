48bde8673cce1a166297e9888e50414cc8640bff
RafaelEmery <rafael.emerycade@gmail.com>
2020-08-26 20:41:54 -0300

    Inicial commit

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    res.send({
+        hello: 'World'
+    });
+});
+
+module.exports = routes;
