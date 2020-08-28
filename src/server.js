b68aea9e48c6678ea58887d73fb7cbe04f2e1c8b
RafaelEmery <rafael.emerycade@gmail.com>
2020-08-28 14:40:55 -0300

    Changed PORT

--- src/server.js
+++ src/server.js
@@ -5,6 +5,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Running on port ', process.env.PORT || 3000);
-});
+app.listen(process.env.PORT || 3000);
