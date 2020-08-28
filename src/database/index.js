236363ca0d5cf38780f16da505cce62aa8884e31
RafaelEmery <rafael.emerycade@gmail.com>
2020-08-28 14:18:01 -0300

    Changed user

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connecting with MongoDB Atlas
-mongoose.connect('mongodb+srv://rafael:rafael@mech-enterprises-db.lvutw.mongodb.net/mech-enterprises-db?retryWrites=true&w=majority', { 
+mongoose.connect('mongodb+srv://deploy:deploy@mech-enterprises-db.lvutw.mongodb.net/mech-enterprises-db?retryWrites=true&w=majority', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }); 
